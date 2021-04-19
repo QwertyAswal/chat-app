9ed60fcf0e1a35af1dcb6babb62dba77be1273ef
QwertyAswal <aswals8991@gmail.com>
2021-04-19 17:30:12 +0530

    added timestamps

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,16 +13,22 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 
-socket.on('locationMessage', (url) => {
-    console.log(url)
-    const html = Mustache.render(locationMessageTemplate, { url })
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
 
 socket.on('message', (message) => {
     console.log(message)
-    const html = Mustache.render(messageTemplate, { message })
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
