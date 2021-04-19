9d128d399b897de27aac7fd04711127debdbf25f
QwertyAswal <aswals8991@gmail.com>
2021-04-19 15:04:46 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicPath))
+
+io.on('connection',()=>{
+    console.log('new ws connection!')
+})
+
+server.listen(port, () => {
+    console.log('Server up at port:-', port)
+})
